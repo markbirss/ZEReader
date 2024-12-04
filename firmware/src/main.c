89e831eb2307426efd837185aacb814a4a1b4d12
Anna-Lena Marx <anna-lena.marx@web.de>
2024-12-04 09:18:03 +0100

    configuration: UART logs and ePaper Display working

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -5,6 +5,7 @@
  */
 
 #include <stdio.h>
+#include <zephyr/logging/log.h>
 #include <lvgl.h>
 #include <lvgl_input_device.h>
 #include <zephyr/device.h>
@@ -12,19 +13,18 @@
 #include <zephyr/drivers/display.h>
 #include <zephyr/kernel.h>
 
-#define LOG_LEVEL CONFIG_LOG_DEFAULT_LEVEL
-#include <zephyr/logging/log.h>
-LOG_MODULE_REGISTER(app);
+LOG_MODULE_REGISTER(main, CONFIG_DISPLAY_LOG_LEVEL);
 
-// struct input_event last_input_event;
+static uint32_t count;
 
 static void hello_world_button_clicked_cb(lv_event_t *e) {
-	LOG_INF("Hello World button clicked with event: %d", e->code);
+	LOG_DBG("Hello World button clicked with event: %d", e->code);
 }
 
 int main(void)
 {
-	printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
+	char count_str[15] = {0};
+	LOG_DBG("Hello World - ZEReader! %s\n", CONFIG_BOARD_TARGET);
 
 	const struct device *display_dev;
 	display_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
@@ -34,19 +34,11 @@ int main(void)
 		return 0;
 	}
 
-	// lv_obj_t *arc;
-	// lv_group_t *arc_group;
-
-	// arc = lv_arc_create(lv_scr_act());
-	// lv_obj_align(arc, LV_ALIGN_CENTER, 0, -15);
-	// lv_obj_set_size(arc, 150, 150);
-
-	// arc_group = lv_group_create();
-	// lv_group_add_obj(arc_group, arc);
+	//display_set_orientation(display_dev, DISPLAY_ORIENTATION_ROTATED_180);	
 	
 	lv_obj_t *hello_world_button;
 	hello_world_button = lv_btn_create(lv_scr_act());
-	lv_obj_align(hello_world_button, LV_ALIGN_BOTTOM_LEFT, 0, 0);
+	lv_obj_align(hello_world_button, LV_ALIGN_CENTER, 0, 0);
 	lv_obj_add_event_cb(hello_world_button, hello_world_button_clicked_cb, LV_EVENT_CLICKED, NULL);
 	
 	lv_obj_t *hello_world_label;
@@ -55,6 +47,18 @@ int main(void)
 	lv_label_set_text(hello_world_label, "My hello world button");
 	lv_obj_align(hello_world_label, LV_ALIGN_CENTER, 0, 0);
 
+	lv_task_handler();
+	display_blanking_off(display_dev);
 
-	return 0;
+	// lv_task_handler();
+
+	// while (1) {
+	// 	if ((count % 100) == 0U) {
+	// 		sprintf(count_str, "Hello %d", count/100U);
+	// 		lv_label_set_text(hello_world_label, count_str);
+	// 	}
+	// 	lv_task_handler();
+	// 	++count;
+	// 	k_sleep(K_MSEC(100));
+	// }
 }
