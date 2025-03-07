3c64639634e032624983bbd2ab710fc386783f24
Anna-Lena Marx <anna-lena.marx@web.de>
2025-03-07 21:05:50 +0100

    feat: Update button mapping and layout for the actual PCB

--- app/src/ui/ui.c
+++ app/src/ui/ui.c
@@ -5,13 +5,15 @@
 
 LOG_MODULE_REGISTER(ui, CONFIG_ZEREADER_LOG_LEVEL);
 
-lv_obj_t *button_left;
-lv_obj_t *button_mid;
-lv_obj_t *button_right;
+lv_obj_t *button_4;
+lv_obj_t *button_3;
+lv_obj_t *button_2;
+lv_obj_t *button_1;
 
-lv_obj_t *button_left_label;
-lv_obj_t *button_mid_label;
-lv_obj_t *button_right_label;
+lv_obj_t *button_4_label;
+lv_obj_t *button_3_label;
+lv_obj_t *button_2_label;
+lv_obj_t *button_1_label;
 
 lv_obj_t *text_area;
 lv_obj_t *logo;
@@ -19,27 +21,35 @@ lv_obj_t *logo;
 void zereader_print_prev_page();
 void zereader_print_next_page();
 
-static void button_left_clicked_cb(lv_event_t *e)
+static void button_1_clicked_cb(lv_event_t *e)
 {
-	LOG_DBG("Left button clicked with event");
+	LOG_DBG("Button 1 clicked with event");
 	context_t *context = lv_event_get_user_data(e);
 	LOG_DBG("Context: %s", context_strings[*context]);
 	LOG_DBG("Event code: %d", lv_event_get_code(e));
 	zereader_print_prev_page();
 }
 
-static void button_mid_clicked_cb(lv_event_t *e)
+static void button_2_clicked_cb(lv_event_t *e)
 {
-	LOG_DBG("Middle button clicked with event");
+	LOG_DBG("Button 2 clicked with event");
 	context_t *context = lv_event_get_user_data(e);
 	LOG_DBG("Context: %s", context_strings[*context]);
 	zereader_clean_page();
 	zereader_clean_logo();
 }
 
-static void button_right_clicked_cb(lv_event_t *e)
+static void button_3_clicked_cb(lv_event_t *e)
 {
-	LOG_DBG("Right button clicked with event");
+	LOG_DBG("Button 3 clicked with event");
+	context_t *context = lv_event_get_user_data(e);
+	LOG_DBG("Context: %s", context_strings[*context]);
+	zereader_show_logo();
+}
+
+static void button_4_clicked_cb(lv_event_t *e)
+{
+	LOG_DBG("Button 4 clicked with event");
 	context_t *context = lv_event_get_user_data(e);
 	LOG_DBG("Context: %s", context_strings[*context]);
 	zereader_print_next_page();
@@ -50,35 +60,44 @@ void zereader_setup_control_buttons(context_t *context)
 	lv_obj_set_style_text_color(lv_screen_active(), lv_color_white(), LV_PART_MAIN);
 
 	LOG_DBG("Button Setup");
-	button_left = lv_button_create(lv_screen_active());
+	button_1 = lv_button_create(lv_screen_active());
 	// lv_obj_add_style(button_left, &style_default, 0);
 	LOG_DBG("Button created");
-	lv_obj_align(button_left, LV_ALIGN_BOTTOM_LEFT, 10, -5);
+	lv_obj_align(button_1, LV_ALIGN_BOTTOM_LEFT, 10, -5);
 	LOG_DBG("Button aligned");
-	lv_obj_add_event_cb(button_left, button_left_clicked_cb, LV_EVENT_CLICKED, context);
+	lv_obj_add_event_cb(button_1, button_1_clicked_cb, LV_EVENT_CLICKED, context);
 	LOG_DBG("Button callback added");
 
-	button_left_label = lv_label_create(button_left);
-	lv_label_set_text(button_left_label, "prev page");
-	lv_obj_center(button_left_label);
+	button_1_label = lv_label_create(button_1);
+	lv_label_set_text(button_1_label, "prev page");
+	lv_obj_center(button_1_label);
 
-	button_mid = lv_button_create(lv_screen_active());
+	button_2 = lv_button_create(lv_screen_active());
 	// lv_obj_add_style(button_mid, &style_default, 0);
-	lv_obj_align(button_mid, LV_ALIGN_BOTTOM_MID, 0, -5);
-	lv_obj_add_event_cb(button_mid, button_mid_clicked_cb, LV_EVENT_CLICKED, context);
+	lv_obj_align(button_2, LV_ALIGN_BOTTOM_MID, -50, -5);
+	lv_obj_add_event_cb(button_2, button_2_clicked_cb, LV_EVENT_CLICKED, context);
 
-	button_mid_label = lv_label_create(button_mid);
-	lv_label_set_text(button_mid_label, "menu");
-	lv_obj_center(button_mid_label);
+	button_2_label = lv_label_create(button_2);
+	lv_label_set_text(button_2_label, "menu");
+	lv_obj_center(button_2_label);
 
-	button_right = lv_button_create(lv_screen_active());
+	button_3 = lv_button_create(lv_screen_active());
 	// lv_obj_add_style(button_right, &style_default, 0);
-	lv_obj_align(button_right, LV_ALIGN_BOTTOM_RIGHT, -10, -5);
-	lv_obj_add_event_cb(button_right, button_right_clicked_cb, LV_EVENT_CLICKED, context);
+	lv_obj_align(button_3, LV_ALIGN_BOTTOM_MID, 50, -5);
+	lv_obj_add_event_cb(button_3, button_3_clicked_cb, LV_EVENT_CLICKED, context);
 
-	button_right_label = lv_label_create(button_right);
-	lv_label_set_text(button_right_label, "next page");
-	lv_obj_center(button_right_label);
+	button_3_label = lv_label_create(button_3);
+	lv_label_set_text(button_3_label, "settings");
+	lv_obj_center(button_3_label);
+
+	button_4 = lv_button_create(lv_screen_active());
+	// lv_obj_add_style(button_right, &style_default, 0);
+	lv_obj_align(button_4, LV_ALIGN_BOTTOM_RIGHT, -10, -5);
+	lv_obj_add_event_cb(button_4, button_4_clicked_cb, LV_EVENT_CLICKED, context);
+
+	button_4_label = lv_label_create(button_4);
+	lv_label_set_text(button_4_label, "next page");
+	lv_obj_center(button_4_label);
 }
 
 void zereader_setup_page()
